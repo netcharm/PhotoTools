aa4f92638fb295fb2fd6cd44a04bea846fe3a4d4
netcharm <netcharm@163.com>
2016-11-04 23:03:05 +0800

    Add Log Form for addin display text info

---
+++ PhotoTool/LogForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace PhotoTool
+{
+    partial class LogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogForm));
+            this.edLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // edLog
+            // 
+            resources.ApplyResources(this.edLog, "edLog");
+            this.edLog.Name = "edLog";
+            // 
+            // LogForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.edLog);
+            this.HelpButton = true;
+            this.Name = "LogForm";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.LogForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edLog;
+    }
+}
---
+++ PhotoTool/LogForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PhotoTool
+{
+    public partial class LogForm : Form
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public LogForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LogForm_Load( object sender, EventArgs e )
+        {
+            #region extracting icon from application to this form window
+            Icon = Icon.ExtractAssociatedIcon( Application.ExecutablePath );
+            #endregion
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        internal void Log(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine( edLog.Text.Trim() );
+            sb.AppendLine( text.Trim() );
+            edLog.Text = sb.ToString();
+        }
+    }
+}
