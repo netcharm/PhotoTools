2bd6bf88af1fc0d15c9cf996c01c04d3e7a611fd
netcharm <netcharm@163.com>
2016-11-05 17:01:07 +0800

    Add Grayscale Effect to InternalFilter

---
+++ Addins/03_Filters/InternalFilters/Effects/GrayscaleForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace InternalFilters.Effects
+{
+    partial class GrayscaleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GrayscaleForm));
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.imgPreview = new NetCharm.Image.Addins.ImageBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imgPreview
+            // 
+            this.imgPreview.Image = null;
+            resources.ApplyResources(this.imgPreview, "imgPreview");
+            this.imgPreview.Name = "imgPreview";
+            this.imgPreview.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.imgPreview.SelectionRegion = ((System.Drawing.RectangleF)(resources.GetObject("imgPreview.SelectionRegion")));
+            this.imgPreview.SizeMode = Cyotek.Windows.Forms.ImageBoxSizeMode.Fit;
+            this.imgPreview.Zoom = 100;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            resources.ApplyResources(this.btnOk, "btnOk");
+            this.btnOk.Name = "btnOk";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnCancel, "btnCancel");
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // GrayscaleForm
+            // 
+            this.AcceptButton = this.btnOk;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.imgPreview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
+            this.Name = "GrayscaleForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip;
+        private NetCharm.Image.Addins.ImageBox imgPreview;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Addins/03_Filters/InternalFilters/Effects/GrayscaleForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NetCharm.Image.Addins;
+
+namespace InternalFilters.Effects
+{
+    public partial class GrayscaleForm : Form
+    {
+        internal AddinHost host;
+        private IAddin addin;
+        private Image thumb = null;
+
+        public GrayscaleForm()
+        {
+            InitializeComponent();
+        }
+
+        public GrayscaleForm( IAddin filter )
+        {
+            this.addin = filter;
+            InitializeComponent();
+
+            toolTip.ToolTipTitle = addin.DisplayName;
+            AddinUtils.Translate( addin, this, toolTip );
+
+            thumb = AddinUtils.CreateThumb( addin.ImageData, imgPreview.Size );
+            imgPreview.Image = thumb;
+        }
+    }
+}
