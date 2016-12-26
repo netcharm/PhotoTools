378beababfd4ad4b6a6b86a8ee013680e655c082
netcharm <netcharm@163.com>
2016-12-26 15:14:38 +0800

    Add Base64png project for Convert between Base64 and PNG Image

---
+++ Base64Png/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Base64Png
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.edBase64 = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPreview
+            // 
+            resources.ApplyResources(this.picPreview, "picPreview");
+            this.picPreview.Name = "picPreview";
+            this.picPreview.TabStop = false;
+            this.toolTip.SetToolTip(this.picPreview, resources.GetString("picPreview.ToolTip"));
+            this.picPreview.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.picPreview_LoadCompleted);
+            this.picPreview.DoubleClick += new System.EventHandler(this.picPreview_DoubleClick);
+            // 
+            // edBase64
+            // 
+            this.edBase64.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.edBase64, "edBase64");
+            this.edBase64.Name = "edBase64";
+            this.edBase64.KeyUp += new System.Windows.Forms.KeyEventHandler(this.edBase64_KeyUp);
+            // 
+            // btnDecode
+            // 
+            resources.ApplyResources(this.btnDecode, "btnDecode");
+            this.btnDecode.Name = "btnDecode";
+            this.toolTip.SetToolTip(this.btnDecode, resources.GetString("btnDecode.ToolTip"));
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnEncode
+            // 
+            resources.ApplyResources(this.btnEncode, "btnEncode");
+            this.btnEncode.Name = "btnEncode";
+            this.toolTip.SetToolTip(this.btnEncode, resources.GetString("btnEncode.ToolTip"));
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // btnPaste
+            // 
+            resources.ApplyResources(this.btnPaste, "btnPaste");
+            this.btnPaste.Name = "btnPaste";
+            this.toolTip.SetToolTip(this.btnPaste, resources.GetString("btnPaste.ToolTip"));
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnCopy
+            // 
+            resources.ApplyResources(this.btnCopy, "btnCopy");
+            this.btnCopy.Name = "btnCopy";
+            this.toolTip.SetToolTip(this.btnCopy, resources.GetString("btnCopy.ToolTip"));
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // toolTip
+            // 
+            this.toolTip.ShowAlways = true;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.edBase64);
+            this.Controls.Add(this.picPreview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.TextBox edBase64;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ Base64Png/MainForm.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Base64Png
+{
+    public partial class MainForm : Form
+    {
+        string title = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="imageSize"></param>
+        /// <param name="patternSize"></param>
+        /// <returns></returns>
+        internal Image MakePatternImage( Size imageSize, int patternSize = 8 )
+        {
+            Bitmap pat = new Bitmap(patternSize*2, patternSize*2, PixelFormat.Format32bppArgb);
+            using ( var g = Graphics.FromImage( pat ) )
+            {
+                g.SmoothingMode = SmoothingMode.HighQuality;
+
+                SolidBrush bb = new SolidBrush(Color.Silver);
+                SolidBrush wb = new SolidBrush(Color.White);
+
+                g.FillRectangle( bb, 0, 0, patternSize, patternSize );
+                g.FillRectangle( bb, patternSize, patternSize, patternSize, patternSize );
+
+                g.FillRectangle( wb, 0, patternSize, patternSize, patternSize );
+                g.FillRectangle( wb, patternSize, 0, patternSize, patternSize );
+            }
+
+            Bitmap bg = new Bitmap(imageSize.Width, imageSize.Height, PixelFormat.Format32bppArgb);
+            using ( var g = Graphics.FromImage( bg ) )
+            {
+                g.SmoothingMode = SmoothingMode.HighQuality;
+
+                TextureBrush pb = new TextureBrush(pat);
+                g.FillRectangle( pb, 0, 0, imageSize.Width, imageSize.Height );
+            }
+            return ( bg );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="base64"></param>
+        /// <returns></returns>
+        internal Image Base64ToImage(string base64)
+        {
+            Image img = null;
+            if ( !string.IsNullOrEmpty( base64 ) )
+            {
+                try
+                {
+                    byte[] arr = Convert.FromBase64String(base64);
+                    using ( MemoryStream ms = new MemoryStream( arr ) )
+                    {
+                        img = Image.FromStream( ms );
+
+                    }
+                }
+                catch(Exception)
+                {
+                        
+                }
+            }
+            return ( img );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="png"></param>
+        /// <returns></returns>
+        internal string ImageToBase64(Image img)
+        {
+            string base64 = string.Empty;
+            if ( img is Image )
+            {
+                try
+                {
+                    using ( MemoryStream ms = new MemoryStream() )
+                    {
+                        img.Save( ms, ImageFormat.Png );
+
+                        byte[] arr = ms.ToArray();
+                        base64 = Convert.ToBase64String( arr, Base64FormattingOptions.InsertLineBreaks );
+                    }
+                }
+                catch ( Exception )
+                {
+
+                }
+            }
+            return ( base64 );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="flist"></param>
+        /// <returns></returns>
+        internal string LoadXML(string[] flist)
+        {
+            string base64 = string.Empty;
+            if ( flist.Length > 0 && File.Exists( flist[0] ) )
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load( flist[0] );
+
+                var names = doc.GetElementsByTagName( "name" );
+                if ( names.Count > 0 )
+                    this.Text = $"{title} - {names[0].Attributes["value"].Value}";
+                else
+                {
+                    var items = doc.GetElementsByTagName( "item" );
+                    if ( items.Count > 0 ) this.Text = $"{title} - {items[0].Attributes["text"].Value}";
+                }
+
+                var thumbs = doc.GetElementsByTagName( "thumbnail" );
+                if ( thumbs.Count > 0 )
+                {
+                    base64 = thumbs[0].InnerText.Trim();
+                }
+            }
+            return ( base64 );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="flist"></param>
+        /// <returns></returns>
+        internal Image LoadImage( string[] flist )
+        {
+            Image img = null;
+            if ( flist.Length > 0 && File.Exists(flist[0]))
+            {
+                using ( FileStream fs = File.OpenRead( flist[0] ) )
+                {
+                    img = Image.FromStream( fs, true, true );
+                }
+            }
+            return ( img );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void AdjustSizeMode()
+        {
+            if ( picPreview.Image.Width > picPreview.ClientSize.Width || picPreview.Image.Height > picPreview.ClientSize.Height )
+                picPreview.SizeMode = PictureBoxSizeMode.Zoom;
+            else
+                picPreview.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load( object sender, EventArgs e )
+        {
+            title = this.Text;
+
+            #region extracting icon from application to this form window
+            Icon = Icon.ExtractAssociatedIcon( Application.ExecutablePath );
+            #endregion
+
+            picPreview.BackgroundImage = MakePatternImage( picPreview.Size );
+        }
+
+        #region DrapDrop Events
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_DragEnter( object sender, DragEventArgs e )
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        /// <summary>
+        ///         
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_DragDrop( object sender, DragEventArgs e )
+        {
+            //string[] txts = { "System.String", "UnicodeText", "Text", "OemText", "Html", "RTF" };
+            //string[] bmps = { "Bitmap", "Dib", "DeviceIndependentBitmap", "DragImageBits" };
+            //string[] files = { "FileDrop", "FileName", "FileNameW", "Shell IDList Array" };
+
+            var formats = e.Data.GetFormats();
+            if ( formats.Contains( "FileContents" ) && formats.Contains( "text/html" ) )
+            {
+                var obj = e.Data.GetData("text/html", true);
+                string html = string.Empty;
+                if ( obj is string )
+                {
+                    html = (string) obj;
+                }
+                else if ( obj is MemoryStream )
+                {
+                    MemoryStream ms = (MemoryStream)obj;
+                    byte[] buffer = new byte[ms.Length];
+                    ms.Read( buffer, 0, (int) ms.Length );
+                    if ( buffer[1] == (byte) 0 )  // Detecting unicode
+                    {
+                        html = Encoding.Unicode.GetString( buffer );
+                    }
+                    else
+                    {
+                        html = Encoding.ASCII.GetString( buffer );
+                    }
+                }
+                // Using a regex to parse HTML, but JUST FOR THIS EXAMPLE :-)
+                //var match = new Regex(@"<img[^/]src=""([^""]*)""").Match(html);
+                var match = new Regex(@"(<img[^/])(.*?src=)(""([^""]*)"")").Match(html);
+                if ( match.Success )
+                {
+                    Uri uri = new Uri(match.Groups[match.Groups.Count-1].Value);
+                    picPreview.ImageLocation = uri.AbsoluteUri;
+                    picPreview.WaitOnLoad = false;
+                    picPreview.LoadAsync();
+                    //picPreview.Load();
+                    //btnEncode.PerformClick();
+                }
+            }
+            else if ( formats.Contains( "Text" ) || formats.Contains( "UnicodeText" ) || formats.Contains( "System.String" ) )
+            {
+                edBase64.Text = (string) e.Data.GetData( "UnicodeText", true );
+                btnDecode.PerformClick();
+            }
+            else if ( formats.Contains( "FileDrop" ) )
+            {
+                string[] flist = (string[])e.Data.GetData( DataFormats.FileDrop, true );
+
+                string[] images = { ".jpg", ".jpeg", ".png", ".tif", ".tiff", ".bmp", ".gif" };
+
+                if ( flist.Length > 0 )
+                {
+                    if ( string.Equals( Path.GetExtension( flist[0] ), ".xml", StringComparison.CurrentCultureIgnoreCase ) )
+                    {
+                        edBase64.Text = LoadXML( flist );
+                        btnDecode.PerformClick();
+                    }
+                    else if ( images.Contains( Path.GetExtension( flist[0] ).ToLower() ) )
+                    {
+                        //picPreview.Image = LoadImage( flist );
+                        picPreview.ImageLocation = flist[0];
+                        picPreview.WaitOnLoad = false;
+                        picPreview.LoadAsync();
+                    }
+                }
+            }
+        }
+        #endregion DragDrop Events
+
+        private void btnDecode_Click( object sender, EventArgs e )
+        {
+            picPreview.WaitOnLoad = false;
+            picPreview.ImageLocation = string.Empty;
+            picPreview.Image = Base64ToImage( edBase64.Text.Trim() );
+            AdjustSizeMode();
+        }
+
+        private void btnEncode_Click( object sender, EventArgs e )
+        {
+            edBase64.Text = ImageToBase64( picPreview.Image );
+        }
+
+        private void btnPaste_Click( object sender, EventArgs e )
+        {
+            if ( Clipboard.ContainsImage() )
+            {
+                picPreview.Image = Clipboard.GetImage();
+                AdjustSizeMode();
+                btnEncode.PerformClick();
+            }
+            else if ( Clipboard.ContainsText() )
+            {
+                edBase64.Text = Clipboard.GetText();
+                btnDecode.PerformClick();
+            }
+        }
+
+        private void btnCopy_Click( object sender, EventArgs e )
+        {
+            Clipboard.SetText( edBase64.Text );
+        }
+
+        private void edBase64_KeyUp( object sender, KeyEventArgs e )
+        {
+            if ( e.Control && e.KeyCode == Keys.A )
+            {
+                edBase64.SelectAll();
+            }
+        }
+
+        private void picPreview_DoubleClick( object sender, EventArgs e )
+        {
+            OpenFileDialog dlgOpen = new OpenFileDialog();
+            dlgOpen.Filter = "Image File (*.jpg;*.png;*.tif;*.bmp;*.gif)|*.jpg;*.jpeg;*.png;*.tif;*.tiff;*.bmp;*.gif";
+            if(dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                picPreview.Image = LoadImage( dlgOpen.FileNames );
+            }
+        }
+
+        private void picPreview_LoadCompleted( object sender, AsyncCompletedEventArgs e )
+        {
+            AdjustSizeMode();
+            btnEncode.PerformClick();
+        }
+
+    }
+}
